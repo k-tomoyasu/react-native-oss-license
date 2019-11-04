bd11e5292b825bca7927a905422b77913dcac8c7
k-tomoyasu <blhicr1303@gmail.com>
2019-11-04 21:35:33 +0900

    separate webpack env

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const merge = require('webpack-merge');
+const base = require('./webpack.config.js');
+const SizePlugin = require('size-plugin');
+
+module.exports = merge(base, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new SizePlugin()
+  ]
+});
