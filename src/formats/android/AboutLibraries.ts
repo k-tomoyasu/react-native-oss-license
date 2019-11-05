4e491a4998e007aa1b9a63c52840e9ff06809dd3
k-tomoyasu <blhicr1303@gmail.com>
2019-11-06 08:54:07 +0900

    fix format

--- src/formats/android/AboutLibraries.ts
+++ src/formats/android/AboutLibraries.ts
@@ -1,45 +1,61 @@
 import Formatter from "../Formatter";
 import License from "../../models/License";
-import YAML from "yaml";
 import fs from "fs-extra";
 
-export default class AboutLibraries implements Formatter {
+export default class AboutLicenses implements Formatter {
     output(licenses: License[]): void {
-        const path = "android/app/src/main/res/values/license_strings.xml"
-        let list = "";
+        const path = "android/app/src/main/res/values/license_npm_libs_strings.xml"
+        let licenseContent = "";
+        let libraryNameList: string[] = [];
         licenses.forEach(license => {
             const libraryName = (license.libraryName + "_" + license.version)
                 .replace(/-/g, "_")
                 .replace(/\./g, "_")
                 .replace(/@/g, "")
                 .replace(/\//g, "");
+            libraryNameList.push(`"${libraryName}"`);
+
             const description = license.description
                 .replace(/'/g, "\\'")
                 .replace(/"/g, '\\"')
                 .replace(/&/g, "&amp;");
-            const detail = `
+            const libraryDetail = `
 <!-- ${license.libraryName} -->
 <string name="define_int_${libraryName}">year;owner</string>
 <string name="library_${libraryName}_author">${license.authorName}</string>
 <string name="library_${libraryName}_libraryName">${license.libraryName}</string>
+<string name="library_${libraryName}_libraryVersion">${license.version}</string>
 <string name="library_${libraryName}_libraryDescription">${description}</string>
 <string name="library_${libraryName}_libraryWebsite">${license.homepage}</string>
 <string name="library_${libraryName}_isOpenSource">true</string>
 <string name="library_${libraryName}_repositoryLink">${license.repositoryUrl}</string>
-<string name="library_${libraryName}_licenseId">${license.license}</string>
 <!-- Custom variables section -->
 <string name="library_${libraryName}_year"></string>
 <string name="library_${libraryName}_owner"></string>`;
-            list += detail;
+            
+            licenseContent += libraryDetail + this.getLicenseDetail(libraryName, license);
         });
+
         const content = 
 `<?xml version="1.0" encoding="utf-8"?>
 <resources>
-    ${list}
+    ${licenseContent}
 </resources>`;
 
         fs.outputFile(path, content)
-            .then(_ => console.log(`output about-libraries format to '${path}'`))
+            .then(_ => console.log(`output about-libraries format to '${path}'.\nwithLibraries(${libraryNameList.join(",")})`))
             .catch(e => console.error(e));
     }
+
+    private getLicenseDetail(libraryName: string, license: License): string {
+        const installedLicense = !!(license.license.match(/apache/i) || license.license.match(/bsd/i) || license.license.match(/mit/i));
+        if (installedLicense) {
+            return `\n<string name="library_${libraryName}_licenseId">${license.license}</string>`
+        }
+        return `
+<string name="library_${libraryName}_licenseContent">${license.licenseContent}</string>
+<string name="library_${libraryName}_licenseVersion">${license.license}</string>
+`
+    }
+
 }
